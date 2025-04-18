f2d89165f0a06971c6ce1f5e09b3202b8b9a98ac
yasna nsh <y.noushirvani@gmail.com>
2025-04-18 22:58:04 +0330

    back bug fix

--- charreader.py
+++ charreader.py
@@ -1,13 +1,14 @@
 class CharReader:
     def __init__(self, filepath):
         self.file = open(filepath, 'r', encoding='utf-8')
-        self.eof_reached = False
+        # self.eof_reached = False
+        self.pos = []
 
     def read(self):
         # if self.eof_reached:
         #     return 'EOF'
-        
-        char = self.file.read(1) 
+        self.pos.append(self.file.tell())
+        char = self.file.read(1)
         # if char == '':
         #     self.eof_reached = True
         #     self.file.close()
@@ -20,6 +21,5 @@ class CharReader:
         #     self.file = open(self.file.name, 'r')
         #     self.file.seek(self.file_size - 1)
         # else:
-        pos = self.file.tell()
-        if pos > 0:
-            self.file.seek(pos - 1)
+        if self.pos:
+            self.file.seek(self.pos.pop())
