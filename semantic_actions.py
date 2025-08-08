98b6496ff99cd7da8e41a44461d3da8dc76f1edc
Raya <rayarezaie83@gmail.com>
2025-08-08 17:20:54 +0330

    added actions enum

--- semantic_actions.py
+++ semantic_actions.py
@@ -1,6 +1,38 @@
 from auxFuncts import *
 from semantic_stack import * 
 from semantic_analayzer import *
+from enum import Enum
+class actionNames(Enum):
+    pid = 0 ,
+    add_or_sub = 1 , 
+    push_ss = 2,
+    dclr_var = 3 ,
+    dclr_arr = 4 ,
+    update_func_params = 5,
+    end_func = 6 ,
+    save_param_list = 7 ,
+    save_param_norm = 8,
+    save_scope = 9,
+    fill_break = 10 ,
+    save_jmp_out_scope = 11,
+    save_if_cond_jpf = 12,
+    fill_if_cond_jpf = 13,
+    fill_if_cond_jpt = 14,
+    loc_while_cond_before = 15,
+    save_while_cond_jpf = 16,
+    fill_while = 17,
+    return_jp = 18,
+    save_return_value = 19,
+    print = 20,
+    assign = 21,
+    calc_arr_addr = 22,
+    relation = 23,
+    push_num_ss =  24,
+    start_args = 25,
+    check_args = 26,
+    calc_arr_addr = 27,
+    mult = 28
+    
 class SemanticAction:
     def __init__(self , type , token):
         self.type = type
